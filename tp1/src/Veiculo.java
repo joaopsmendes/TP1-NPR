cd36b2cbb21a9f02b09e5cf2c39665d0b9807596
Rui Alves <ruipedrolousada@gmail.com>
2023-03-16 20:39:52 +0000

    Update Veiculo.java

--- tp1/src/Veiculo.java
+++ tp1/src/Veiculo.java
@@ -1,3 +1,39 @@
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
 public class Veiculo {
 
+    Timer cTimer; //timer used to receive data from the UDP socket
+    byte[] cBuf; //buffer used to store data received from the server
+
+    private DatagramSocket socketEnviar;
+    private DatagramSocket socketReceber;
+
+    public veiculo(InetAddress ipRSU) throws SocketException {
+
+        this.socketEnviar = new DatagramSocket(4000);
+        this.socketReceber = new DatagramSocket(4321);
+
+        new Thread(() -> { // 
+            try {
+
+                Packet p = new Packet();
+                DatagramPacket request = new DatagramPacket(p.serialize(), p.serialize().length, ipserver, 4321);
+                socketEnviar.send(request);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
